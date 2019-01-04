577f63b8a36a422297aab79046e643f3f916635f
BT_1N3_15@bktech.local <BT_1N3_15@bktech.local>
2019-01-04 11:42:56 -0500

    Initial commit

---
+++ src/sample/Main.java
@@ -0,0 +1,32 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Main extends Application {
+
+    public boolean gamerunning = true;
+    public int buttoninput;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+
+}
---
+++ src/sample/SampleController.java
@@ -0,0 +1,13 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
+public class SampleController {
+    public Label button1;
+    public Label button2;
+
+    public void sayHelloWorld(ActionEvent actionEvent) {
+        helloWorld.setText("Hello World!");
+    }
+}
