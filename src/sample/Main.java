0b7db2feeb46202b0c989180652bd971d21decae
BT_1N3_15 <dwong00023@bths.edu>
2019-01-08 11:41:21 -0500

    commitdidbetter22

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,10 +11,6 @@ import java.awt.event.ActionListener;
 
 public class Main extends Application {
 
-    public static boolean gamerunning = true;
-    public static int[] answers = new int[50];
-    private static int indexnow = 0;
-
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
@@ -24,20 +20,7 @@ public class Main extends Application {
     }
 
 
-    public static void main(String[] args) {
-        launch(args);
-        while(gamerunning == true)
-        {
-            int binput = SampleController.returnbutton();
-            System.out.println(binput);
-            if(binput != answers[indexnow])
-            {
-                gamerunning = false;
-            }
-        }
-
-        SampleController.endgame(gamerunning);
-    }
+    public static void main(String[] args) { launch(args); }
 
 
 
--- src/sample/SampleController.java
+++ src/sample/SampleController.java
@@ -14,16 +14,36 @@ public class SampleController {
     private static Label test;
     private static int buttoninput;
 
-    public static void endgame(boolean gamerunning) {
+    public static boolean gamerunning = true;
+    public static int[] answers = new int[50];
+    private static int indexnow = 0;
+
+    public void rungame() {
+        for(int i = 0; i < answers.length;i++)
+        {
+            answers[i] = (int)Math.random()*4+1;
+        }
+        while (gamerunning == true)
+        {
+            int binput = SampleController.returnbutton();
+            System.out.println(binput);
+            if (binput != answers[indexnow]) {
+                gamerunning = false;
+            }
+        }
+
         if(gamerunning==true)
-        {
-            test.setText("You WON");
-        }
+        { test.setText("You WON"); }
         else
-        {
-            test.setText("You LOST");
-        }
+        { test.setText("You LOST"); }
     }
+
+
+
+
+
+
+
 
     public static int returnbutton() {
         return buttoninput;
@@ -36,30 +56,31 @@ public class SampleController {
     public void setButton1()
     {
         buttoninput = 1;
-        //System.out.println(buttoninput);
+        System.out.println(buttoninput);
+        for(int i = 0; i < answers.length;i++)
+        {
+            answers[i] = (int)Math.floor(Math.random()*4+1);
+            System.out.println(answers[i]);
+        }
+
     }
 
     public void setButton2()
     {
         buttoninput = 2;
-        //System.out.println(buttoninput);
+        System.out.println(buttoninput);
     }
 
     public void setButton3()
     {
         buttoninput = 3;
-        //System.out.println(buttoninput);
+        System.out.println(buttoninput);
     }
 
     public void setButton4()
     {
         buttoninput = 4;
-        //System.out.println(buttoninput);
-    }
-
-    public int sendToCheck()
-    {
-        return this.buttoninput;
+        System.out.println(buttoninput);
     }
 
     //will change these later to be one thing with a parameter
